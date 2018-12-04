cc931e9167fc8349c86d2590095da53316a5a2db
Bhumesh <bhumesh@Pavans-Mac-mini.local>
2018-12-04 19:05:35 +0530

    implemented the functionality

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var cors = require('cors');
+var express = require("express");
+var cors = require("cors");
+var multer = require("multer");
+
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
 
 // require and use "multer"...
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get("/", function(req, res) {
+	res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+app.post("/api/fileanalyse", upload.single("upfile"), function(req, res) {
+	console.log(req.file);
+	res.json({ name: req.file.originalname, type: req.file.mimetype, size: req.file.size });
+});
+
+app.listen(process.env.PORT || 3000, function() {
+	console.log("Node.js listening ...");
 });
